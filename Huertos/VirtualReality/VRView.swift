150afa9d32c661dcd2db7b4a43723be97a8d5602
GeorginaZeron <georgina.zeronca@udlap.mx>
2024-03-07 12:50:53 -0600

    vr2

--- Huertos/VirtualReality/VRView.swift
+++ Huertos/VirtualReality/VRView.swift
@@ -1,14 +1,35 @@
 import UIKit
+import SwiftUI
 import RealityKit
 import ARKit
 
-class YourARViewController: UIViewController {
-    @IBOutlet var arView: ARView!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        //let anchor = try! Experience.loadBox()
-        //arView.scene.anchors.append(anchor)
+struct PotViewPlant: UIViewRepresentable{
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    func makeUIView(context: Context) -> some UIView {
+        let arView = ARView()
+        
+        // Start AR session
+        let session = arView.session
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.vertical]
+        session.run(config)
+        
+        // Load the .reality file's scene asynchronously
+        /*
+        Pro.loadPotAsync { result in
+            switch result {
+            case .success(let anchor):
+                // Add the loaded scene to the ARView
+                arView.scene.anchors.append(anchor)
+            case .failure(let error):
+                // Handle errors here
+                print("Error loading scene: \(error.localizedDescription)")
+            }
+        }
+        */
+        return arView
     }
 }
