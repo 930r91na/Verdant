5b84c6c173eb28a61fdcac5ea1637126bc2428ca
Nina Maeva Mazadiego Cortes <mazcortmae@gmail.com>
2024-03-06 19:43:04 -0600

    Geo toda la tarde

--- Huertos/Views/Plant/PlantCardView.swift
+++ Huertos/Views/Plant/PlantCardView.swift
@@ -1,91 +1,18 @@
+//
+//  PlantCardView.swift
+//  Huertos
+//
+//  Created by Maeva Mazadiego on 06/03/24.
+//
+
 import SwiftUI
 
 struct PlantCardView: View {
-    var plant: Plant
-
-    @State private var navigateToPlantView = false
-    
     var body: some View {
-        Button(action: {
-            navigateToPlantView = true
-        }) {
-            VStack {
-                Spacer()
-                NavigationLink(destination: PlantView(plant: plant), isActive: $navigateToPlantView) { EmptyView() }
-                Image(systemName: "leaf.circle.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 40, height: 40)
-                    .foregroundColor(.primaryGreen)
-                
-                Spacer()
-                
-                Text(plant.alias)
-                    .font(.system(size: 12, weight: .bold))
-                    .foregroundColor(.black)
-                    .fixedSize(horizontal: false, vertical: true)
-                    .frame(alignment: .center)
-                
-                Spacer()
-            }
-            .padding(10)
-            .frame(width: 100, height: 100)
-            .background(Color.white)
-            .cornerRadius(12)
-            .shadow(color: .gray.opacity(0.4), radius: 8, x: 0, y: 2)
-        }
-        .buttonStyle(PlainButtonStyle())
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct TreeCardView: View {
-    var tree: Tree
-
-    @State private var navigateToTreeView = false
-    
-    var body: some View {
-        Button(action: {
-            navigateToTreeView = true
-        }) {
-            VStack (alignment: .center) {
-                Spacer()
-                
-                NavigationLink(destination: TreeView(tree: tree), isActive: $navigateToTreeView) { EmptyView() }
-                Image(systemName: "tree.circle.fill") // Ícono sugerido para árboles, siéntete libre de cambiarlo
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 40, height: 40)
-                    .foregroundColor(.primaryGreen)
-                
-                Spacer()
-
-                Text(tree.alias)
-                    .font(.system(size: 12, weight: .bold))
-                    .foregroundColor(.black)
-                    .fixedSize(horizontal: false, vertical: true)
-                    .frame(alignment: .center)
-                
-                Spacer()
-            }
-            .padding(10)
-            .frame(width: 100, height: 100)
-            .background(Color.white)
-            .cornerRadius(12)
-            .shadow(color: .gray.opacity(0.4), radius: 8, x: 0, y: 2)
-        }
-        .buttonStyle(PlainButtonStyle())
-    }
+#Preview {
+    PlantCardView()
 }
-
-
-/*
-struct PlantCardView_Previews: PreviewProvider {
-    static var previews: some View {
-        let plantExample: Plant
-        
-        PlantCardView(plant: plantExample)
-            .previewLayout(.sizeThatFits)
-            .padding()
-    }
-}
-*/
