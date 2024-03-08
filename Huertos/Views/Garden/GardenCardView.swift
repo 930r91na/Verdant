6655f7276f6fd8e665eabed27d413ae8e43dda96
Nina Maeva Mazadiego Cortes <mazcortmae@gmail.com>
2024-03-08 04:10:56 -0600

    Fixed card click

--- Huertos/Views/Garden/GardenCardView.swift
+++ Huertos/Views/Garden/GardenCardView.swift
@@ -6,32 +6,32 @@ struct GardenCardView: View {
     @State private var selectedGarden: Garden? // Para la navegación
 
     var body: some View {
-        ZStack {
-            // Imagen de fondo del jardín
-            garden.gardenpic
-                .resizable()
-                .aspectRatio(contentMode: .fill)
+        // Contenido de la tarjeta
+        Button(action: {
+            self.selectedGarden = garden
+            navigateToGardenView = true
+        }) {
+            ZStack {
+                // Imagen de fondo del jardín
+                garden.gardenpic
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 351, height: 165)
+                    .cornerRadius(12)
+                    .clipped()
+                
+                // Gradiente sobre la imagen de fondo
+                LinearGradient(
+                    stops: [
+                        .init(color: Color(red: 0.38, green: 0.42, blue: 0.22).opacity(0.5), location: 0.00),
+                        .init(color: Color(red: 0.4, green: 0.4, blue: 0.4).opacity(0.5), location: 1.00),
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
                 .frame(width: 351, height: 165)
                 .cornerRadius(12)
-                .clipped()
-
-            // Gradiente sobre la imagen de fondo
-            LinearGradient(
-                stops: [
-                    .init(color: Color(red: 0.38, green: 0.42, blue: 0.22).opacity(0.5), location: 0.00),
-                    .init(color: Color(red: 0.4, green: 0.4, blue: 0.4).opacity(0.5), location: 1.00),
-                ],
-                startPoint: .top,
-                endPoint: .bottom
-            )
-            .frame(width: 351, height: 165)
-            .cornerRadius(12)
-            
-            // Contenido de la tarjeta
-            Button(action: {
-                self.selectedGarden = garden
-                navigateToGardenView = true
-            }) {
+                
                 VStack(alignment: .leading, spacing: 5) {
                     HStack{
                         HStack{
@@ -61,7 +61,7 @@ struct GardenCardView: View {
                     }
                     
                     Spacer()
-
+                    
                     HStack {
                         Image(systemName: "location.fill")
                             .foregroundColor(.white)
@@ -75,12 +75,12 @@ struct GardenCardView: View {
                     .cornerRadius(40)
                 }
                 .padding()
+                .frame(width: 351, height: 165)
+                .cornerRadius(12)
+                .shadow(color: .black.opacity(0.1), radius: 16, x: 0, y: 0)
             }
-            .frame(width: 351, height: 165)
-            .cornerRadius(12)
-            .shadow(color: .black.opacity(0.1), radius: 16, x: 0, y: 0)
+            .frame(width: 351, height: 165, alignment: .leading)
         }
-        .frame(width: 351, height: 165, alignment: .leading)
         .background(NavigationLink(destination: GeneralGardenView(garden: selectedGarden ?? garden), isActive: $navigateToGardenView) {
             EmptyView()
         })
