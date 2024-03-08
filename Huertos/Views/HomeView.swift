421b27c8adfb8289389137534491152c997257fd
Nina Maeva Mazadiego Cortes <mazcortmae@gmail.com>
2024-03-08 04:37:29 -0600

    Start of homeview

--- Huertos/Views/HomeView.swift
+++ Huertos/Views/HomeView.swift
@@ -2,14 +2,26 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var isShowingNotification = false
+    var name: String = "Megan"
     
     var body: some View {
         NavigationView {
             ScrollView{
-                VStack{
-                    Text("Home view")
-                    NavigationLink(destination: NotificationView(), isActive: $isShowingNotification) { EmptyView() }
+                NavigationLink(destination: NotificationView(), isActive: $isShowingNotification) { EmptyView() }
+                HStack {
+                    VStack(alignment: .leading){
+                        Text("Hello \(name),")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                            
+                        Text("Welcome back!")
+                            .font(.title2)
+                            .foregroundColor(.primaryGreen)
+                    }
+                    Spacer()
                 }
+                .padding()
                 .navigationTitle("Home")
                 .navigationBarItems(trailing: Button(action: {
                     isShowingNotification = true
