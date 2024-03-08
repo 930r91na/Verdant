3c59375170c51f612d5bd03f9ab3f21c07243ee7
Nina Maeva Mazadiego Cortes <mazcortmae@gmail.com>
2024-03-08 13:28:47 -0600

    Design for plant stages

--- Huertos/Views/Plant/PlantStagesView.swift
+++ Huertos/Views/Plant/PlantStagesView.swift
@@ -43,37 +43,177 @@ struct TomatoStageDetailView: View {
             HStack(spacing: 20) {
                 ForEach(Array(stages.enumerated()), id: \.element.id) { (index, stage) in
                     VStack(alignment: .leading, spacing: 5) {
-                        Text(stage.stageName.rawValue)
-                            .font(.headline)
+                        HStack{
+                            Image("tomatoStage\(index)")
+                                .resizable()
+                                .frame(width: 260, height: 165, alignment: .center)
+                                .background(
+                                LinearGradient(
+                                    stops: [
+                                    Gradient.Stop(color: Color(red: 0.38, green: 0.42, blue: 0.22).opacity(0.5), location: 0.00),
+                                    Gradient.Stop(color: Color(red: 0.4, green: 0.4, blue: 0.4).opacity(0.5), location: 1.00),
+                                    ],
+                                    startPoint: UnitPoint(x: 0.5, y: 0),
+                                    endPoint: UnitPoint(x: 0.5, y: 1)
+                                    )
+                                )
+                                .cornerRadius(12)
+                        }
+                        .padding(.bottom, 10)
                         
-                        Text("Duration: \(stage.duration)")
-                        Text("Description: \(stage.description)")
-                        Text("Water Requirement: \(stage.waterRequirement)")
-                        Text("Sunlight Requirement: \(stage.sunlightRequirement.0) to \(stage.sunlightRequirement.1) hours")
-                        Text("Soil Composition: \(stage.soilComposition)")
-                        Text("Volume: \(stage.volume)L")
-                        Text("Draining Requirement: \(stage.drainingRequirement ? "Yes" : "No")")
+                        Text(stage.stageName.rawValue)
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        
+                        VStack(alignment: .leading, spacing: 0) {
+                            Text("Duration:")
+                                .font(
+                                    Font.custom("SF Pro Display", size: 14)
+                                    .weight(.semibold)
+                                    .italic()
+                                )
+                                .foregroundColor(.primaryGreen)
+                            
+                            Text("\(stage.duration)")
+                                .font(Font.custom("SF Pro Display", size: 11))
+                                .foregroundColor(.black)
+                        }
+                        .padding(10)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color(red: 0.38, green: 0.42, blue: 0.22).opacity(0.15))
+                        .cornerRadius(12)
+                        
+                        VStack(alignment: .leading, spacing: 0) {
+                            Text("Description:")
+                                .font(
+                                    Font.custom("SF Pro Display", size: 14)
+                                    .weight(.semibold)
+                                    .italic()
+                                )
+                                .foregroundColor(.primaryGreen)
+                            
+                            Text("\(stage.description)")
+                                .font(Font.custom("SF Pro Display", size: 11))
+                                .foregroundColor(.black)
+                        }
+                        .padding(10)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color(red: 0.38, green: 0.42, blue: 0.22).opacity(0.15))
+                        .cornerRadius(12)
+                        
+                        VStack(alignment: .leading, spacing: 0) {
+                            Text("Water Requirement:")
+                                .font(
+                                    Font.custom("SF Pro Display", size: 14)
+                                    .weight(.semibold)
+                                    .italic()
+                                )
+                                .foregroundColor(.primaryGreen)
+                            
+                            Text("\(stage.waterRequirement)")
+                                .font(Font.custom("SF Pro Display", size: 11))
+                                .foregroundColor(.black)
+                        }
+                        .padding(10)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color(red: 0.38, green: 0.42, blue: 0.22).opacity(0.15))
+                        .cornerRadius(12)
+                        
+                        VStack(alignment: .leading, spacing: 0) {
+                            Text("Sunlight Requirement:")
+                                .font(
+                                    Font.custom("SF Pro Display", size: 14)
+                                    .weight(.semibold)
+                                    .italic()
+                                )
+                                .foregroundColor(.primaryGreen)
+                            
+                            if stage.sunlightRequirement.1 == 0{
+                                Text("0 hours")
+                                    .font(Font.custom("SF Pro Display", size: 11))
+                                    .foregroundColor(.black)
+                            }
+                            else{
+                                Text("\(Int(stage.sunlightRequirement.0.rounded())) to \(Int(stage.sunlightRequirement.1.rounded())) hours")
+                                    .font(Font.custom("SF Pro Display", size: 11))
+                                    .foregroundColor(.black)
+                            }
+
+                        }
+                        .padding(10)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color(red: 0.38, green: 0.42, blue: 0.22).opacity(0.15))
+                        .cornerRadius(12)
+                        
+                        VStack(alignment: .leading, spacing: 0) {
+                            Text("Soil Composition:")
+                                .font(
+                                    Font.custom("SF Pro Display", size: 14)
+                                    .weight(.semibold)
+                                    .italic()
+                                )
+                                .foregroundColor(.primaryGreen)
+                            
+                            Text("\(stage.soilComposition)")
+                                .font(Font.custom("SF Pro Display", size: 11))
+                                .foregroundColor(.black)
+                        }
+                        .padding(10)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color(red: 0.38, green: 0.42, blue: 0.22).opacity(0.15))
+                        .cornerRadius(12)
+                        
+                        VStack(alignment: .leading, spacing: 0) {
+                            Text("Volume:")
+                                .font(
+                                    Font.custom("SF Pro Display", size: 14)
+                                    .weight(.semibold)
+                                    .italic()
+                                )
+                                .foregroundColor(.primaryGreen)
+                            
+                            Text("\(Int(stage.volume.rounded()))L")
+                                .font(Font.custom("SF Pro Display", size: 11))
+                                .foregroundColor(.black)
+                        }
+                        .padding(10)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color(red: 0.38, green: 0.42, blue: 0.22).opacity(0.15))
+                        .cornerRadius(12)
+                        
+                        VStack(alignment: .leading, spacing: 0) {
+                            Text("Draining Requirement:")
+                                .font(
+                                    Font.custom("SF Pro Display", size: 14)
+                                    .weight(.semibold)
+                                    .italic()
+                                )
+                                .foregroundColor(.primaryGreen)
+                            
+                            Text("\(stage.drainingRequirement ? "Yes" : "No")")
+                                .font(Font.custom("SF Pro Display", size: 11))
+                                .foregroundColor(.black)
+                        }
+                        .padding(10)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color(red: 0.38, green: 0.42, blue: 0.22).opacity(0.15))
+                        .cornerRadius(12)
+                        
                         if let tempRange = stage.temperatureRange {
                             Text("Temperature Range: \(tempRange.0)°C to \(tempRange.1)°C")
                         }
                         Text("Root Depth: \(stage.rootDepth)cm")
                         Text("Spacing: \(stage.spacing)cm")
-                        
-                        // Usamos el índice para construir el nombre de la imagen
-                        Image("tomatoStage\(index)")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(height: 200)
-                        
                         Text("Notes: \(stage.notes)")
                     }
                     .padding()
-                    .background(Color.white) // Fondo de cada tarjeta, ajusta a tus necesidades
-                    .cornerRadius(10) // Esquinas redondeadas para cada tarjeta
-                    .shadow(radius: 5) // Sombra para dar efecto elevado a cada tarjeta
+                    .frame(width: 300, height: 700)
+                    .background(Color.white)
+                    .cornerRadius(12)
+                    .shadow(radius: 5)
                 }
             }
-            .padding(.horizontal) // Espaciado horizontal para que no esté pegado a los bordes
+            .padding()
         }
     }
 }
